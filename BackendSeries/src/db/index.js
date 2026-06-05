dbc6466ed5fa27b152096beade6d76d98491bba5
komal-rathore-100105 <24.a09.027@gmail.com>
2026-06-05 23:30:45 +0530

    changes

---
+++ BackendSeries/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const mongoUrl = process.env.MONGODB_URL?.replace(/\/+$/, "")
+        const connectionInstance = await mongoose.connect(`${mongoUrl}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} \n`)
+    } catch (error) {
+        console.log("MONGODB connection error", error);
+        process.exit(1);
+    }
+}
+export default connectDB;
---
+++ BackendSeries/src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({ path: './env' })
+import dotenv from "dotenv";
+
+
+import connectDB from "./db/index.js";
+
+dotenv.config()
+connectDB()
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URL}`)
+        app.on("error", (error) => {
+            console.log("ERRor: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    }
+    catch (error) {
+        console.error("Error: ", error)
+        throw error
+    }
+})()
+*/
